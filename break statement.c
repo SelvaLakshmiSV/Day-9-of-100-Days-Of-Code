2f4170b02f4f2e0d885f45ba78c217cd1e12217d
Selva Lakshmi S.V <76725996+SelvaLakshmiSV@users.noreply.github.com>
2021-04-26 22:19:01 +0530

    BREAK Staement

---
+++ break statement.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+  int number;
+  printf("Enter number : ");
+  scanf("%d",&number);
+  while(n!=0)
+  {
+     if(number<0)
+       break;
+     printf("%d",number);
+  }
+
+return 0;
+}
