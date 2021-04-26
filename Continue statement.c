a03dabb8b3cad546223076565e582ca3cfa32f05
Selva Lakshmi S.V <76725996+SelvaLakshmiSV@users.noreply.github.com>
2021-04-26 22:27:40 +0530

    COntinue statement

---
+++ Continue statement.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+  int n = 2;
+  for(int i = 1;i<=20;i++)
+  {
+      if(i = n)
+      {
+          n = n+2;
+          continue;
+      }
+      printf("%d",i);
+  }
+}
