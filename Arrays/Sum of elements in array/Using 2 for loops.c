8bf22d8561e64943fb1179c2e9aaed40f7a30b01
Selva Lakshmi S.V <76725996+SelvaLakshmiSV@users.noreply.github.com>
2021-04-26 23:04:20 +0530

    Sum of Array elements

---
+++ Arrays/Sum of elements in array/Using 2 for loops.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+   // Array of 5 floating elements
+   float arr[5], sum=0.0; 
+   int i;
+
+   printf("Enter array elements:\n");
+
+   //  take 5 number
+   for(i=0;i<5;i++)
+   {
+     scanf("%f",&arr[i]);
+   }
+
+   // add that 5 number
+   for(i=0;i<5;i++)
+   {
+     sum += arr[i]; //sum=sum+arr[i]
+   }
+
+   printf("Sum of Array elements are: %f", sum);
+
+   return 0;
+}
